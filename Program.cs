4eeb25bc7580623eaf9015c83071103f36815544
Alireza <Alireza@DESKTOP-GF6SLMD>
2025-08-03 20:40:59 +0330

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace EmailConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
